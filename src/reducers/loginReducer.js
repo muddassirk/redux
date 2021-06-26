18e9b3084dba86ae2e578f03bb92aeaa573f0590
muddassirk <mkkhanu72@gmail.com>
2021-06-27 01:58:11 +0500

    reducers

---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,13 @@
+
+const loginReducer =  (state = false , action) => {
+    switch (action.type) {
+
+    case "LOGIN":
+        return !state;
+
+    default:
+        return state
+    }
+}
+
+export default loginReducer
